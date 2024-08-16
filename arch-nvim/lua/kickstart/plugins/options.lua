da3c9a4cfe687cc8686068bec388167a8b32ae05
wsnugroho <wisnunugroho614@gmail.com>
2024-08-16 21:14:51 +0700

    chore: adding neovim config from my arch machine

---
+++ arch-nvim/lua/kickstart/plugins/options.lua
@@ -0,0 +1,18 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.timeoutlen = 300
+vim.opt.updatetime = 250
+vim.opt.undofile = true
+
+vim.opt.showmode = false
+vim.opt.signcolumn = 'yes'
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.cmdheight = 0
+vim.opt.pumheight = 10
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
